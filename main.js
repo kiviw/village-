d79b2b88a2dd266d79953900a74e153f7f86b281
kiviw <100179542+kiviw@users.noreply.github.com>
2023-07-10 15:04:36 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// Constants
+const villageMapElement = document.getElementById('village-map');
+const entityInfoElement = document.getElementById('entity-info');
+const historyLogElement = document.getElementById('history-log');
+const startButton = document.getElementById('start-btn');
+const pauseButton = document.getElementById('pause-btn');
+
+// Rest of the code...
